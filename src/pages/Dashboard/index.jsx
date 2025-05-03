0a42a4a2336a63d7e7abcbfda01f9f07898a9ac4
iamluhmrt <luizhenrique.business95@gmail.com>
2025-05-02 23:22:12 -0300

    feat: firebase login

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,5 +1,47 @@
+// src/pages/Dashboard/index.jsx
+import { useEffect, useState } from 'react';
+import { auth, db } from '../../firebase/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
+
 function DashboardPage() {
-  return <h1>Dashboard Page</h1>;
+  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const user = auth.currentUser;
+
+      if (!user) {
+        navigate('/');
+        return;
+      }
+
+      const userDoc = await getDoc(doc(db, 'users', user.uid));
+      if (userDoc.exists()) {
+        setUserData(userDoc.data());
+      }
+    };
+
+    fetchUserData();
+  }, [navigate]);
+
+  if (!userData) return <p>Loading user data...</p>;
+
+  return (
+    <div className='container'>
+      <h1>Welcome!</h1>
+      <p>
+        <strong>Name:</strong> {userData.firstName}
+      </p>
+      <p>
+        <strong>Surname:</strong> {userData.lastName}
+      </p>
+      <p>
+        <strong>Birth Date:</strong> {userData.birthDate}
+      </p>
+    </div>
+  );
 }
 
 export default DashboardPage;
